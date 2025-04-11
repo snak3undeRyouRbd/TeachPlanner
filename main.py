ba624635026509454b395ae11ff3f2261478fe22
scarabey35 <den.zzz168@gmail.com>
2025-04-11 17:49:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def init_db():
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
         )
     ''')
     conn.commit()
